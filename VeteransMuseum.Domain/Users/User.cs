5cb29ac7cb3da42ccac392e6ffb6ac562cf71c74
Diamond <valiullinoff@gmail.com>
2025-04-19 17:59:08 +0500

    AddUser

---
+++ VeteransMuseum.Domain/Users/User.cs
@@ -0,0 +1,40 @@
+using VeteransMuseum.Domain.Abstractions;
+
+namespace VeteransMuseum.Domain.Users;
+
+public class User : Entity
+{
+    private User(
+        Guid id,
+        FirstName firstName,
+        LastName lastName,
+        Email email) 
+        : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+
+    private User() { }
+    
+    public FirstName FirstName { get; private set; }
+    
+    public LastName LastName { get; private set; }
+    
+    public Email Email { get; set; }
+    
+    public string IdentityId { get; private set; } = string.Empty;
+    
+    public static User Create(FirstName firstName, LastName lastName, Email email)
+    {
+        var user = new User(Guid.NewGuid(), firstName, lastName, email);
+        
+        return user;
+    }
+    
+    public void SetIdentityId(string identityId)
+    {
+        IdentityId = identityId;
+    }
+}
--- VeteransMuseum.WebApi/Extensions/SeedDataExtensions.cs
+++ VeteransMuseum.WebApi/Extensions/SeedDataExtensions.cs
@@ -2,6 +2,7 @@
 using Bogus;
 using Dapper;
 using VeteransMuseum.Application.Abstractions.Data;
+using VeteransMuseum.Domain.Shared;
 
 namespace VeteransMuseum.WebApi.Extensions;
 
@@ -15,19 +16,26 @@ internal static class SeedDataExtensions
             using IDbConnection connection = sqlConnectionFactory.CreateConnection();
 
             var faker = new Faker();
+            
+            List<object> news = new();
+            for (int i = 0; i < 100; i++)
+            {
+                news.Add(new
+                {
+                    Id = Guid.NewGuid(),
+                    Title = faker.Company.CompanyName(),
+                    Content = faker.Random.String(500),
+                    ImageUrl = faker.Image.PicsumUrl(),
+                    CreatedAt = DateTime.Now,
+                    CrearedBy = faker.Person.FullName
+                });
+            }
 
-            // List<object> apartments = new();
-            // for (int i = 0; i < 100; i++)
-            // {
-            //
-            // }
-            //
-            // const string sql = """
-            // INSERT INTO public.apartments
-            // (id, "name", description, address_country, address_state, address_zip_code, address_city, address_street, price_amount, price_currency, cleaning_fee_amount, cleaning_fee_currency, amenities, last_booked_on_utc)
-            // VALUES(@Id, @Name, @Description, @Country, @State, @ZipCode, @City, @Street, @PriceAmount, @PriceCurrency, @CleaningFeeAmount, @CleaningFeeCurrency, @Amenities, @LastBookedOn);
-            // """;
-            //
-            // connection.Execute(sql, apartments);
+            const string sql = """
+               INSERT INTO public.news
+               (id, title, "content", image_url, created_at, created_by)
+               VALUES(@Id, @Title, @Content, @ImageUrl, @CreatedAt, @CreatedBy);
+           """;
+            
         }
     }
